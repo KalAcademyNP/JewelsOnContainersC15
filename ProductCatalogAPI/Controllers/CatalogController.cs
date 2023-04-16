f136746a1b323b52948d8bb4123a092259267d1e
Kal <info@kalacademy.org>
2023-04-16 12:33:51 -0700

    Added more APIs to the catalog microservice

--- ProductCatalogAPI/Controllers/CatalogController.cs
+++ ProductCatalogAPI/Controllers/CatalogController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProductCatalogAPI.Data;
+using ProductCatalogAPI.Domain;
+using ProductCatalogAPI.ViewModels;
 
 namespace ProductCatalogAPI.Controllers
 {
@@ -10,9 +12,11 @@ namespace ProductCatalogAPI.Controllers
     public class CatalogController : ControllerBase
     {
         private readonly CatalogContext _context;
-        public CatalogController(CatalogContext context)
+        private readonly IConfiguration _config;
+        public CatalogController(CatalogContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
         [HttpGet("[action]")]
@@ -27,6 +31,74 @@ namespace ProductCatalogAPI.Controllers
         {
             var brands = await _context.CatalogBrands.ToListAsync();
             return Ok(brands);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Items(
+            [FromQuery] int pageIndex = 0, 
+            [FromQuery] int pageSize = 6)
+        {
+            var itemsCount = _context.Catalog.LongCountAsync();
+            var items = await _context.Catalog
+                                .OrderBy(c => c.Name)
+                                .Skip(pageIndex * pageSize)
+                                .Take(pageSize)
+                                .ToListAsync();
+            items = ChangePictureUrl(items);
+
+            var model = new PaginatedItemsViewModel
+            {
+                PageIndex = pageIndex,
+                PageSize = items.Count,
+                Data = items,
+                Count = itemsCount.Result
+            };
+
+            return Ok(model);
+        }
+
+        [HttpGet("[action]/filter")]
+        public async Task<IActionResult> Items(
+            [FromQuery] int? catalogTypeId,
+            [FromQuery] int? catalogBrandId,
+            [FromQuery] int pageIndex = 0,
+            [FromQuery] int pageSize = 6)
+        {
+            var query = (IQueryable<CatalogItem>) _context.Catalog;
+            if (catalogTypeId.HasValue)
+            {
+                query = query.Where(c => c.CatalogTypeId == catalogTypeId.Value);
+            }
+            if (catalogBrandId.HasValue)
+            {
+                query = query.Where(c => c.CatalogBrandId == catalogBrandId.Value);
+            }
+
+            var itemsCount = query.LongCountAsync();
+            var items = await query
+                                .OrderBy(c => c.Name)
+                                .Skip(pageIndex * pageSize)
+                                .Take(pageSize)
+                                .ToListAsync();
+            items = ChangePictureUrl(items);
+
+            var model = new PaginatedItemsViewModel
+            {
+                PageIndex = pageIndex,
+                PageSize = items.Count,
+                Data = items,
+                Count = itemsCount.Result
+            };
+
+            return Ok(model);
+        }
+
+        private List<CatalogItem> ChangePictureUrl(List<CatalogItem> items)
+        {
+            items.ForEach(item => item.PictureUrl = item.PictureUrl
+                        .Replace("http://externalcatalogbaseurltobereplaced",
+                        _config["ExternalBaseUrl"]));
+            return items;
         }
     }
 }
