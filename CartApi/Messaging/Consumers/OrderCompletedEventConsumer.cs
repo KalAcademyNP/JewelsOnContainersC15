0704f57d1b14cefc37c47cf362209579b6c1b28d
Kal <info@kalacademy.org>
2023-05-20 12:14:39 -0700

    Messaging between order and cart

---
+++ CartApi/Messaging/Consumers/OrderCompletedEventConsumer.cs
@@ -0,0 +1,19 @@
+﻿using CartApi.Data;
+using Common.Messaging;
+using MassTransit;
+
+namespace CartApi.Messaging.Consumers
+{
+    public class OrderCompletedEventConsumer : IConsumer<OrderCompletedEvent>
+    {
+        private readonly ICartRepository _repository;
+        public OrderCompletedEventConsumer(ICartRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task Consume(ConsumeContext<OrderCompletedEvent> context)
+        {
+            await _repository.DeleteCartAsync(context.Message.BuyerId);
+        }
+    }
+}
